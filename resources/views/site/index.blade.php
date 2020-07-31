ab07524eb38043a4b3ebfd0849fbd908647068cd
Wesley Menezes <wesleymgarcia@bol.com.br>
2020-07-30 22:11:48 -0300

    Ajuste no index e no css

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -214,7 +214,7 @@
                     <br /><br /><br />
 
                     <div class="row">
-                        <div class="col-md-4" >
+                        <div class="col-md-4" style="background-color: #fff;">
                             <div class="item-box-blog">
 
                                 <div class="item-box-blog-image">
@@ -248,7 +248,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4" >
+                        <div class="col-md-4" style="background-color: #fff;">
                             <div class="item-box-blog">
 
                                 <div class="item-box-blog-image">
@@ -282,7 +282,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4" >
+                        <div class="col-md-4" style="background-color: #fff;">
                             <div class="item-box-blog">
 
                                 <div class="item-box-blog-image">
